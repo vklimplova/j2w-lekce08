86d8bdfea0fea7cfc35f1dc47e72be74d3c07988
Filip Jirsák <filip@jirsak.org>
2021-05-17 00:46:06 +0200

    Entita má ID generované databází.

--- src/main/java/cz/czechitas/java2webapps/lekce8/entity/Osoba.java
+++ src/main/java/cz/czechitas/java2webapps/lekce8/entity/Osoba.java
@@ -4,6 +4,8 @@ import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
@@ -20,6 +22,7 @@ import java.time.LocalDate;
 @Entity
 public class Osoba {
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   @Length(max = 100)
